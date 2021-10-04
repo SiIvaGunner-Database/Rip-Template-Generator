c4a0b2dae6e3c9a7a2ec1795e2288203e4bb35a9
AAAAAAidan <a.k.zamboni@gmail.com>
2021-10-04 16:12:16 -0700

    Update background color

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const fs = require("fs");
-const server = http.createServer((request, response) => {
+
+http.createServer((request, response) => {
   response.statusCode = 200;
   response.setHeader("Content-Type", "text/html");
   fs.readFile("./views/index.html", (error, data) => {
